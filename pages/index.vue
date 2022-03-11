feb44f54a7b4a616b3a2f2df3e36c34695799197
shahrokh <shahrokhelmi@gmail.com>
2022-03-11 22:23:05 +0330

    add pages and components folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <div v-for="home in homes">
-        {{home.title}}
+    <div v-for="home in homes" :key="home.objectID" style="float: left; margin:10px">
+        <home-card :home="home"></home-card>
     </div>
   </div>
 </template>
 
 <script>
-import home from '../data/homes.json';
-
-export default{
-  data(){
-    return{
-      homes: home.slice(0,3),
+import home from '~/data/homes';
+import HomeCard from "~/components/HomeCard";
+export default {
+  name: "index",
+  components:{
+    HomeCard
+  },
+  data() {
+    return {
+      homes: home.slice(0, 3),
     }
   }
 }
-
 </script>
-
 
 
 <!--<script>-->
