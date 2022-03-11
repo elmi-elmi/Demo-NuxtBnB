419ada0bd23c6ad2be48186688c8dc46eebfc8f7
shahrokh <shahrokhelmi@gmail.com>
2022-03-11 21:51:24 +0330

    init

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="home in homes">
+        {{home.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+import home from '../data/homes.json';
+
+export default{
+  data(){
+    return{
+      homes: home.slice(0,3),
+    }
+  }
+}
+
+</script>
+
+
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "index"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
