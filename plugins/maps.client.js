54f3f216a6c8f70c359b29cc3115b18028b57439
shahrokh <shahrokhelmi@gmail.com>
2022-03-21 16:11:03 +0330

    rebuild showMap

--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -1,37 +1,47 @@
 export default function (context, inject) {
     console.log("maps.client started...........");
 
-    let mapLoaded = false;
-    let mapWaiting = null;
+    let isMapLoaded = false;
+    let mapWaiting = [];
 
     addScript();
     inject("maps", {showMap});
 
-    function initMap() {
-        mapLoaded = true;
-        if (mapWaiting) {
-            const {canvas, lat, lng} = mapWaiting;
-            renderMap(canvas, lat, lng);
-            mapWaiting = null;
-        }
+    function initGoogleMap() {
+        // isMapLoaded = true;
+        // if (mapWaiting) {
+        //     const {canvas, lat, lng} = mapWaiting;
+        //     renderMap(canvas, lat, lng);
+        //     mapWaiting = null;
+        // }
+        mapWaiting.forEach(item=>{
+            isMapLoaded = true;
+            if(typeof item.fn === 'function') {
+                item.fn(...item.arguments)
+            }
+        })
     }
 
     function addScript() {
         const script = document.createElement("script");
         script.src =
-            "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU&callback=initMap";
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU&callback=initGoogleMap";
         script.async = true;
-        // script.skip = process.client && mapLoaded;
-        window.initMap = initMap;
+        // script.skip = process.client && isMapLoaded;
+        window.initGoogleMap = initGoogleMap;
         document.head.appendChild(script);
     }
 
     function showMap(canvas, lat, lng) {
-        if (mapLoaded) renderMap(canvas, lat, lng);
-        else mapWaiting = {canvas, lat, lng};
-    }
-
-    function renderMap(canvas, lat, lng) {
+        // if (isMapLoaded) renderMap(canvas, lat, lng);
+        // else mapWaiting = {canvas, lat, lng};
+        if(!isMapLoaded){
+            mapWaiting.push({
+                fn:showMap,
+                arguments
+            })
+            return
+        }
         const uluru = {lat, lng};
         const map = new google.maps.Map(canvas, {
             center: uluru,
@@ -44,4 +54,18 @@ export default function (context, inject) {
             map: map,
         });
     }
+
+    // function renderMap(canvas, lat, lng) {
+    //     const uluru = {lat, lng};
+    //     const map = new google.maps.Map(canvas, {
+    //         center: uluru,
+    //         zoom: 8,
+    //     });
+    //
+    //     // The marker, positioned at Uluru
+    //     const marker = new google.maps.Marker({
+    //         position: uluru,
+    //         map: map,
+    //     });
+    // }
 }
