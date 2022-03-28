52a6d99d7c12e68fc128de1b9bea13aeeb7004f7
shahrokh <shahrokhelmi@gmail.com>
2022-03-28 19:32:28 +0430

    property reveiws & host

---
+++ components/PropertyReviews.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app-padded-vertical">
+    <div class="app-testimonials">
+      <div v-for="(review, index) in reviews" :key="index">
+        <div class="app-testimonial-header">
+          <div>
+            <img :src="review.reviewer.image" alt="review" />
+          </div>
+          <div>
+            <p class="app-testimonial-name">
+              {{ review.reviewer.name }}
+            </p>
+            <p class="app-testimonial-date">
+              {{ shortDate(review.date) }}
+            </p>
+          </div>
+        </div>
+        <div class="app-testimonial-body">
+          <ShortText :text="review.comment" :target="150"/>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import shortDate from "~/utils/shortDate";
+export default {
+  name: "PropertyReviews",
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    shortDate,
+  },
+};
+</script>
+
+<style scoped></style>
--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -5,56 +5,15 @@
       <PropertyDetails :home="home" />
       <PropertyDescription :home="home"/>
       <PropertyMap :home="home"/>
-    </div>
-    <div style="display: flex">
-      <img
-          v-for="image in home.images"
-          :src="image"
-          alt="home"
-          :key="image"
-          width="200"
-          height="150"
-      />
+      <PropertyReviews :reviews="reviews"/>
+      <PropertyHost :user="user"/>
     </div>
 
-    {{ home.title }}<br/>
-    ${{ home.pricePerNight }} / night <br/>
-    <img
-        src="../../static/images/marker.svg"
-        alt="address"
-        height="20"
-        width="20"
-    />
-    {{ home.location.address }} {{ home.location.city }}
-    {{ home.location.state }}   {{ home.location.country }} <br/>
-    <img
-        src="../../static/images/star.svg"
-        alt="address"
-        height="20"
-        width="20"
-    />
-    {{ home.reviewValue }} <br/>
-    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }},
-    {{ home.bathrooms }} bath<br/>
-    {{ home.description }}<br/>
-    <div style="height: 330px; width: 330px" ref="map"></div>
-    <div v-for="review in reviews">
-      <img :src="review.reviewer.image" alt="reviewer"/> <br/>
-      {{ review.reviewer.name }} <br/>
-      {{ review.rating }} <br/>
-      {{ review.date }} <br/>
-      <ShortText :text="review.comment" :target="200"/>
-    </div>
-    <!--    <img :src="home.host.image" height="100" />-->
-    <img :src="user.image" alt="user"/> <br/>
-    {{ user.name }} <br/>
-    {{ formatData(user.joined) }}
-    {{ user.reviewCount }} <br/>
-    {{ user.description }}
   </div>
 </template>
 
 <script>
+import shortDate from "../../utils/shortDate";
 export default {
   name: "id",
   components: {},
@@ -67,26 +26,11 @@ export default {
   },
 
   methods: {
-    formatData(dateStr) {
-      const date = new Date(dateStr);
-      return date.toLocaleDateString(undefined, {
-        month: "long",
-        year: "numeric",
-      });
-    },
+    shortDate
   },
 
-  mounted() {
-    console.log("mounted  _id.vue");
-    this.$maps.showMap(
-        this.$refs.map,
-        this.home._geoloc.lat,
-        this.home._geoloc.lng
-    );
-  },
 
   async asyncData({$dataApi, params, error}) {
-    console.log("asyncData _id.vue fire........");
     const homeId = params.id;
     const responses = await Promise.all([
       $dataApi.getHome(homeId),
@@ -96,37 +40,9 @@ export default {
     const badResponse = responses.find((response) => !response.ok);
     if (badResponse)
       return error({
-        statusCode: homeResponse.status,
-        message: homeResponse.statusText,
+        statusCode: badResponse.status,
+        message: badResponse.statusText,
       });
-    // const homeId = params.id;
-    // const homeResponse = await $dataApi.getHome(homeId);
-    // if (!homeResponse.ok) {
-    //   console.log(
-    //     "asyncData error",
-    //     homeResponse.status,
-    //     homeResponse.statusText
-    //   );
-    //   return error({
-    //     statusCode: homeResponse.status,
-    //     message: homeResponse.statusText,
-    //   });
-    // }
-    //
-    // const reviewResponse = await $dataApi.getReviewByHomeId(homeId);
-    // if (!reviewResponse.ok)
-    //   return error({
-    //     statusCode: reviewResponse.status,
-    //     message: reviewResponse.statusText,
-    //   });
-    //
-    // const userResponse = await $dataApi.getUserByHomeId(homeId);
-    // if (!userResponse) {
-    //   return error({
-    //     statusCode: reviewResponse.status,
-    //     message: userResponse.statusText,
-    //   });
-    // }
 
     return {
       home: responses[0].json,
